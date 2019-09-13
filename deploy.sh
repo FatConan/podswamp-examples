4d200e460fd10ec12a7d3939d9159a3f1565fb93
Ian Usher <iancapacitated@gmail.com>
2019-09-13 22:59:21 +0100

    Setting options

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-scp -i deploy_key -r spy/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/spy.pod/
-scp -i deploy_key -r blacktapes/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/blacktapes.pod/
-scp -i deploy_key -r nightvale/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/nightvale.pod/
-scp -i deploy_key -r deadauthors/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/deadauthors.pod
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r spy/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/spy.pod/
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r blacktapes/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/blacktapes.pod/
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r nightvale/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/nightvale.pod/
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r deadauthors/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/deadauthors.pod
