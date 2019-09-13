bd46c577c354118171e0cba9e04400d5c2e47d7d
Ian Usher <iancapacitated@gmail.com>
2019-09-13 23:41:27 +0100

    Correcting rsync calls

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo "   User $DEPLOY_USER" >> $HOME/.ssh/config
 echo "   StrictHostKeyChecking no" >> $HOME/.ssh/config
 echo "   IdentityFile $TRAVIS_BUILD_DIR/deploy_key" >> $HOME/.ssh/config
 
-rsync -avh spy/html/* $DEPLOY_NAME:~/public_html/spy.pod/
-rsync -avh blacktapes/html/* $DEPLOY_NAME:~/public_html/blacktapes.pod/
-rsync -avh spy/html/* $DEPLOY_NAME nightvale/html/* $DEPLOY_NAME:~/public_html/nightvale.pod/
-rsync -avh spy/html/* $DEPLOY_NAME deadauthors/html/* $DEPLOY_NAME:~/public_html/deadauthors.pod/
+rsync -arv spy/html/* $DEPLOY_NAME:~/public_html/spy.pod/
+rsync -arv blacktapes/html/* $DEPLOY_NAME:~/public_html/blacktapes.pod/
+rsync -arv nightvale/html/* $DEPLOY_NAME:~/public_html/nightvale.pod/
+rsync -arv deadauthors/html/* $DEPLOY_NAME:~/public_html/deadauthors.pod/
