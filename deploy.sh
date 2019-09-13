12bd7da7fbebbc4dcc133ec5724a8d1cce4f2df9
Ian Usher <iancapacitated@gmail.com>
2019-09-13 23:36:49 +0100

    Trying to set up an ssh config for rsync

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,12 @@
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r spy/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/spy.pod/
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r blacktapes/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/blacktapes.pod/
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r nightvale/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/nightvale.pod/
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i deploy_key -r deadauthors/html/* $DEPLOY_USER@$DEPLOY_HOST:~/public_html/deadauthors.pod
+touch $HOME/.ssh/config
+echo "" >> $HOME/.ssh/config
+echo "Host $DEPLOY_NAME" >> $HOME/.ssh/config
+echo "   HostName $DEPLOY_HOST" >> $HOME/.ssh/config
+echo "   User $DEPLOY_USER" >> $HOME/.ssh/config
+echo "   StrictHostKeyChecking no" >> $HOME/.ssh/config
+echo "   IdentityFile $TRAVIS_BUILD_DIR/deploy_key" >> $HOME/.ssh/config
+
+rsync -avh spy/html/* $DEPLOY_NAME:~/public_html/spy.pod/
+rsync -avh blacktapes/html/* $DEPLOY_NAME:~/public_html/blacktapes.pod/
+rsync -avh spy/html/* $DEPLOY_NAME nightvale/html/* $DEPLOY_NAME:~/public_html/nightvale.pod/
+rsync -avh spy/html/* $DEPLOY_NAME deadauthors/html/* $DEPLOY_NAME:~/public_html/deadauthors.pod/
